c83cddcf5105a4474ccd75bbecbc3c03cf8b6187
kmanish9301 <manishkharbade@gmail.com>
2024-09-09 22:05:14 +0530

    Deployment env file changes

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,11 +1,30 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: parseInt(import.meta.env.PORT) || 5173,
-    host: true,
-  },
-});
+export default ({ mode }) => {
+  // Load environment variables
+  const env = loadEnv(mode, process.cwd());
+
+  // Set base name for the project (can come from env)
+  const base = env.VITE_BASE_NAME || "/main";
+
+  // Use Render's PORT env variable, or fallback to 5174 for local dev
+  const port = env.PORT ? parseInt(env.VITE_PORT, 10) : 5174;
+
+  return defineConfig({
+    base,
+    test: {
+      global: true,
+      environment: "jsdom",
+    },
+    plugins: [react()],
+    server: {
+      host: "0.0.0.0", // Expose on network for deployment
+      port, // Use Render's PORT or fallback to 5174
+    },
+    build: {
+      target: "esnext",
+      chunkSizeWarningLimit: 500,
+    },
+  });
+};
