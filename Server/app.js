ae1a343d730f0755c7b3e8361c6e7515cdcd04ab
kmanish9301 <manishkharbade@gmail.com>
2024-09-10 15:50:22 +0530

    Changes made

--- Server/app.js
+++ Server/app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // Serve static files directly from the root directory
-app.use(express.static(path.join(__dirname, "uploads")));
+app.use(express.static(path.join(__dirname, "../../uploads")));
 app.use(express.static(path.join(__dirname, "userImages")));
 
 // Routes
